5bd7884161584082d125e2cf77c2d806ac46afe5
PJ Frias <peejfrias@gmail.com>
2018-02-20 20:01:35 +0000

    automatically backed up by learn

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -8,7 +8,11 @@ class PostsController < ApplicationController
   end
 
   def update
-    @post.update(post_params)
+    @post.category = post_params(:category)
+    @post.content = post_params(:content)
+    @post.title = post_params(:title)
+    if @post.valid?
+      @post.update(post_params)
 
     redirect_to post_path(@post)
   end
